999f0d52323258fc9bdcecf72c9229322cb5e839
conrad.alex <conrad.alex@539e7f98-af51-0410-a57d-6d84e64ae090>
2008-07-18 17:37:27 +0000

    added kicking capabilities and refactored for full registry usage
    git-svn-id: http://supybot-badwords.googlecode.com/svn/trunk@37 539e7f98-af51-0410-a57d-6d84e64ae090

--- Badwords/config.py
+++ Badwords/config.py
@@ -39,34 +39,26 @@ def configure(advanced):
     from supybot.questions import expect, anything, something, yn
     conf.registerPlugin('Badwords', True)
 
-
 Badwords = conf.registerPlugin('Badwords')
 # This is where your configuration variables (if any) should go.  For example:
+conf.registerChannelValue(Badwords, 'words', registry.CommaSeparatedListOfStrings([], """A comma-separated list of words."""))
+conf.registerChannelValue(Badwords, 'kicks', registry.SpaceSeparatedListOfStrings([], """A comma-separated list of words."""))
+
 conf.registerGroup(Badwords, "response")
-conf.registerGlobalValue(Badwords.response, 'string', registry.String("PLEASE, USE A MORE APPROPRIATE VOCABULARY!", """The response message returned to the user when a word is used."""))
+conf.registerChannelValue(Badwords.response, 'string', registry.String("PLEASE, USE A MORE APPROPRIATE VOCABULARY!", """The response message returned to the user when a word is used."""))
 conf.registerGlobalValue(Badwords.response, 'asNotice', registry.Boolean(True, """The response returned as notice."""))
 conf.registerGlobalValue(Badwords.response, 'asPrivate', registry.Boolean(True, """The response returned as private."""))
 
-# Confirmation messages
-conf.registerGroup(Badwords, "confirmation")
-conf.registerGlobalValue(Badwords.confirmation, 'response', registry.String("Message response set: '%(response)s'", """The confirmation message returned when adding words."""))
-conf.registerGlobalValue(Badwords.confirmation, 'add', registry.String("Word(s) added for %(channel)s.", """The confirmation message returned when adding words."""))
-conf.registerGlobalValue(Badwords.confirmation, 'remove', registry.String("Word(s) removed for %(channel)s.", """The confirmation message returned when removing words."""))
-conf.registerGlobalValue(Badwords.confirmation, 'clear', registry.String("All words cleared for %(channel)s.", """The confirmation message returned when clearing words."""))
-conf.registerGlobalValue(Badwords.confirmation, 'clearAll', registry.String("All words cleared for all channels.", """The confirmation message returned when clearing words of all channels."""))
-conf.registerGlobalValue(Badwords.confirmation, 'asNotice', registry.Boolean(True, """The confirmation message returned as notice."""))
-conf.registerGlobalValue(Badwords.confirmation, 'asPrivate', registry.Boolean(True, """The confirmation message returned as private."""))
-
 # Listing
-conf.registerGroup(Badwords, "listing")
-conf.registerGlobalValue(Badwords.listing, 'asNotice', registry.Boolean(True, """The word listing returned as notice."""))
-conf.registerGlobalValue(Badwords.listing, 'asPrivate', registry.Boolean(True, """The word listing returned as private."""))
+conf.registerGroup(Badwords, "list")
+conf.registerGlobalValue(Badwords.list, 'asNotice', registry.Boolean(True, """The word listing returned as notice."""))
+conf.registerGlobalValue(Badwords.list, 'asPrivate', registry.Boolean(True, """The word listing returned as private."""))
 
 # Forwarding
 conf.registerGroup(Badwords, "forwarding")
-conf.registerGlobalValue(Badwords.forwarding, 'enabled', registry.Boolean(False, """Enables channel forwarding if set to True."""))
-conf.registerGlobalValue(Badwords.forwarding, 'channels', registry.SpaceSeparatedListOfStrings([], """A space-separated list of channels."""))
-conf.registerGlobalValue(Badwords.forwarding, 'string', registry.String("User %(nick)s from %(channel)s said: %(message)s", """The forwarding message sent to channels."""))
+conf.registerChannelValue(Badwords.forwarding, 'enabled', registry.Boolean(False, """Enables channel forwarding if set to True."""))
+conf.registerChannelValue(Badwords.forwarding, 'channels', registry.SpaceSeparatedListOfStrings([], """A space-separated list of channels."""))
+conf.registerChannelValue(Badwords.forwarding, 'string', registry.String("User %(nick)s from %(channel)s said: %(message)s", """The forwarding message sent to channels."""))
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
--- Badwords/plugin.py
+++ Badwords/plugin.py
@@ -32,13 +32,12 @@ import supybot.utils as utils
 from supybot.commands import *
 import supybot.plugins as plugins
 import supybot.ircutils as ircutils
+import supybot.ircmsgs as ircmsgs
 import supybot.callbacks as callbacks
 import supybot.conf as conf
 
 import re
 import fnmatch
-import cPickle
-import os.path
 
 def string_to_wordlist(words):
     """Return a list of words from the given comma-separated-string."""
@@ -53,100 +52,20 @@ class Badwords(callbacks.Plugin):
     This should describe *how* to use this plugin."""
     threaded = True
 
-    BADWORDS_DATA = os.path.join(conf.supybot.directories.data(), "badwords.pkl")
-
     def __init__(self, irc):
         self.__parent = super(Badwords, self)
         self.__parent.__init__(irc)
-
-        # Loads self.words from disk.
-        self.load()
 
         # A cache for precompiled regular expression
         # regex[word] = regex
         self.regex = {}
 
-    def load(self):
-        """Load the self.words from disk."""
-        if not os.path.isfile(Badwords.BADWORDS_DATA):
-            self.words = {}
-            return
-        f = open(Badwords.BADWORDS_DATA, "rb")
-        self.words = cPickle.load(f)
-        f.close()
-
-    def save(self):
-        """Save the self.words to disk."""
-        f = open(Badwords.BADWORDS_DATA, "wb")
-        cPickle.dump(self.words, f)
-        f.close()
-
-    def _get_responseString(self): return conf.supybot.plugins.Badwords.response.string()
-    def _set_responseString(self, msg): conf.supybot.plugins.Badwords.response.string.setValue(msg)
-    responseString = property(_get_responseString, _set_responseString)
-
-    def _get_responseAsNotice(self): return conf.supybot.plugins.Badwords.response.asNotice()
-    def _set_responseAsNotice(self, msg): conf.supybot.plugins.Badwords.response.asNotice.setValue(msg)
-    responseAsNotice = property(_get_responseAsNotice, _set_responseAsNotice)
-
-    def _get_responseAsPrivate(self): return conf.supybot.plugins.Badwords.response.asPrivate()
-    def _set_responseAsPrivate(self, msg): conf.supybot.plugins.Badwords.response.asPrivate.setValue(msg)
-    responseAsPrivate = property(_get_responseAsPrivate, _set_responseAsPrivate)
-
-    def _get_confirmationResponse(self): return conf.supybot.plugins.Badwords.confirmation.response()
-    def _set_confirmationResponse(self, msg): conf.supybot.plugins.Badwords.confirmation.setValue(msg)
-    confirmationResponse = property(_get_confirmationResponse, _set_confirmationResponse)
-
-    def _get_confirmationAdd(self): return conf.supybot.plugins.Badwords.confirmation.add()
-    def _set_confirmationAdd(self, msg): conf.supybot.plugins.Badwords.confirmation.add.setValue(msg)
-    confirmationAdd = property(_get_confirmationAdd, _set_confirmationAdd)
-
-    def _get_confirmationRemove(self): return conf.supybot.plugins.Badwords.confirmation.remove()
-    def _set_confirmationRemove(self, msg): conf.supybot.plugins.Badwords.confirmation.remove.setValue(msg)
-    confirmationRemove = property(_get_confirmationRemove, _set_confirmationRemove)
-
-    def _get_confirmationClear(self): return conf.supybot.plugins.Badwords.confirmation.clear()
-    def _set_confirmationClear(self, msg): conf.supybot.plugins.Badwords.confirmation.clear.setValue(msg)
-    confirmationClear = property(_get_confirmationClear, _set_confirmationClear)
-
-    def _get_confirmationClearAll(self): return conf.supybot.plugins.Badwords.confirmation.clearAll()
-    def _set_confirmationClearAll(self, msg): conf.supybot.plugins.Badwords.confirmation.clearAll.setValue(msg)
-    confirmationClearAll = property(_get_confirmationClearAll, _set_confirmationClearAll)
-
-    def _get_confirmationAsNotice(self): return conf.supybot.plugins.Badwords.confirmation.asNotice()
-    def _set_confirmationAsNotice(self, msg): conf.supybot.plugins.Badwords.confirmation.asNotice.setValue(msg)
-    confirmationAsNotice = property(_get_confirmationAsNotice, _set_confirmationAsNotice)
-
-    def _get_confirmationAsPrivate(self): return conf.supybot.plugins.Badwords.confirmation.asPrivate()
-    def _set_confirmationAsPrivate(self, msg): conf.supybot.plugins.Badwords.confirmation.asPrivate.setValue(msg)
-    confirmationAsPrivate = property(_get_confirmationAsPrivate, _set_confirmationAsPrivate)
-
-    def _get_listingAsNotice(self): return conf.supybot.plugins.Badwords.listing.asNotice()
-    def _set_listingAsNotice(self, msg): conf.supybot.plugins.Badwords.listing.asNotice.setValue(msg)
-    listingAsNotice = property(_get_listingAsNotice, _set_listingAsNotice)
-
-    def _get_listingAsPrivate(self): return conf.supybot.plugins.Badwords.listing.asPrivate()
-    def _set_listingAsPrivate(self, msg): conf.supybot.plugins.Badwords.listing.asPrivate.setValue(msg)
-    listingAsPrivate = property(_get_listingAsPrivate, _set_listingAsPrivate)
-
-    def _get_forwardingEnabled(self): return conf.supybot.plugins.Badwords.forwarding.enabled()
-    def _set_forwardingEnabled(self, msg): conf.supybot.plugins.Badwords.forwarding.enabled.setValue(msg)
-    forwardingEnabled = property(_get_forwardingEnabled, _set_forwardingEnabled)
-
-    def _get_forwardingChannels(self): return conf.supybot.plugins.Badwords.forwarding.channels()
-    def _set_forwardingChannels(self, msg): conf.supybot.plugins.Badwords.forwarding.channels.setValue(msg)
-    forwardingChannels = property(_get_forwardingChannels, _set_forwardingChannels)
-
-    def _get_forwardingString(self): return conf.supybot.plugins.Badwords.forwarding.string()
-    def _set_forwardingString(self, msg): conf.supybot.plugins.Badwords.forwarding.string.setValue(msg)
-    forwardingString = property(_get_forwardingString, _set_forwardingString)
-
-    def add(self, irc, msg, args, channel, word):
-        """[<channel>] <word>, <word>, ...
+    def add(self, irc, msg, args, channel, optlist, words):
+        """[<channel>] [--kick] <word>, <word>, ...
 
         Add one or more words from the given <word> list (comma-separated) for
         channel <channel>. If <channel> is not given, add words for the current
-        channel.
+        channel. If --kick is given, words will be set for user kicking.
 
         Support for Unix shell-style wildcards is available:
         *	matches everything.
@@ -158,19 +77,25 @@ class Badwords(callbacks.Plugin):
         (backup/restore).
         """
 
-        if not channel in self.words:
-            self.words[channel] = []
+        kick = False
+        for (option, arg) in optlist:
+            if option == 'kick':
+                kick = True
 
-        for word in string_to_wordlist(word):
-            if word not in self.words[channel]:
-                self.words[channel].append(word)
+        wordlist = self.registryValue('words', channel=channel)
+        kicklist = self.registryValue('kicks', channel=channel)
 
-        self.save()
+        for word in string_to_wordlist(words):
+            if word not in wordlist:
+                wordlist.append(word)
+                kicklist.append(kick)
+            else:
+                i = wordlist.index(word)
+                kicklist[i] = kick
+        irc.replySuccess()
+    add = wrap(add, ['channel', getopts({'kick':''}), 'text', 'admin'])
 
-        return irc.reply(self.confirmationAdd % {"channel":channel}, private=self.confirmationAsPrivate, notice=self.confirmationAsNotice)
-    add = wrap(add, ['channel', 'text', 'admin'])
-
-    def remove(self, irc, msg, args, channel, word):
+    def remove(self, irc, msg, args, channel, words):
         """[<channel>] <word>, <word>, ...
 
         Remove one or more words from the given <word> list (comma-separated)
@@ -178,13 +103,15 @@ class Badwords(callbacks.Plugin):
         current channel.
         """
 
-        for word in string_to_wordlist(word):
-            if word in self.words.get(channel, []):
-                self.words[channel].remove(word)
+        wordlist = self.registryValue('words', channel=channel)
+        kicklist = self.registryValue('kicks', channel=channel)
 
-        self.save()
-
-        return irc.reply(self.confirmationRemove % {"channel":channel}, private=self.confirmationAsPrivate, notice=self.confirmationAsNotice)
+        for word in string_to_wordlist(words):
+            if word in wordlist:
+                i = wordlist.index(word)
+                del wordlist[i]
+                del kicklist[i]
+        irc.replySuccess()
     remove = wrap(remove, ['channel', 'text', 'admin'])
 
     def response(self, irc, msg, args, message):
@@ -193,19 +120,46 @@ class Badwords(callbacks.Plugin):
         Set <messsage> as a response to bad word abusers. If <message> is not
         given, return the current message.
         """
+
         if message is not None:
-            self.responseString = message
-        return irc.reply(self.confirmationResponse % {"response":self.responseString}, private=self.confirmationAsPrivate, notice=self.confirmationAsNotice)
+            self.setRegistryValue('response.string', message)
+            irc.replySuccess()
+        else:
+            irc.reply(self.registryValue('response.string'))
     response = wrap(response, [optional('text'), 'admin'])
 
-    def list(self, irc, msg, args, channel):
-        """[<channel>]
+    def list(self, irc, msg, args, channel, optlist):
+        """[<channel>] [--{kick,notice}]
 
-        Show the list of words.
+        Show the list of all words for channel <channel>. If <channel> is not
+        given, list all words for the current channel. If --kick is given,
+        list only kicking words. If --notice is given, list only notice
+        (non-kicking) words.
         """
 
-        return irc.reply("%s: %s" % (channel, ", ".join(self.words.get(channel, []))), private=self.listingAsPrivate, notice=self.listingAsNotice)
-    list = wrap(list, ['channel', 'admin'])
+        wordlist = self.registryValue('words', channel=channel)
+        kicklist = self.registryValue('kicks', channel=channel)
+
+        kick = None
+        notice = None
+
+        for (option, arg) in optlist:
+            if option == 'kick':
+                kick = True
+            elif option == 'notice':
+                notice = True
+
+        # This means no option was given.
+        if kick is None and notice is None:
+            notice, kick = True, True
+
+        show = []
+        for i, word in enumerate(wordlist):
+            kicklist[i] and kick and show.append(word)
+            not kicklist[i] and notice and show.append(word)
+
+        irc.reply("%s: %s" % (channel, ", ".join(show)), private=self.registryValue('list.asPrivate'), notice=self.registryValue('list.asNotice'))
+    list = wrap(list, ['channel', getopts({'kick':'', 'notice':''}), 'admin'])
 
     def clear(self, irc, msg, args, channel):
         """[<channel>]
@@ -214,24 +168,22 @@ class Badwords(callbacks.Plugin):
         given, clear words from the current channel.
         """
 
-        if channel in self.words:
-            del self.words[channel][:]
-            self.regex.clear()
-            self.save()
-        return irc.reply(self.confirmationClear % {"channel":channel}, private=self.confirmationAsPrivate, notice=self.confirmationAsNotice)
+        wordlist = self.setRegistryValue('words', [], channel=channel)
+        kicklist = self.setRegistryValue('kicks', [], channel=channel)
+        irc.replySuccess()
     clear = wrap(clear, ['channel', 'admin'])
 
-    def clearall(self, irc, msg, args):
-        """
-
-        Clear all stored words of all channels.
-        """
-
-        self.words.clear()
-        self.regex.clear()
-        self.save()
-        return irc.reply(self.confirmationClearAll, private=self.confirmationAsPrivate, notice=self.confirmationAsNotice)
-    clearall = wrap(clearall, ['admin'])
+    # FIXME: Make this thing work when we figure out how to clear the 'words'
+    # value from all channels in the registry.
+#    def clearall(self, irc, msg, args):
+#        """
+#
+#        Clear all stored words of all channels.
+#        """
+#
+#        self.regex.clear()
+#        irc.replySuccess()
+#    clearall = wrap(clearall, ['admin'])
 
     def doPrivmsg(self, irc, msg):
         """This is called everytime an IRC message is recieved."""
@@ -245,10 +197,17 @@ class Badwords(callbacks.Plugin):
 
         # Do we have any words for the channel?
         channel = msg.args[0]
-        if not channel in self.words:
+
+        wordlist = self.registryValue('words', channel=channel)
+        kicklist = self.registryValue('kicks', channel=channel)
+
+        if not wordlist:
             return
 
-        for word in self.words[channel]:
+        # This is a dict containing {'word':True} as the word string and its kick value.
+        words = dict(zip(wordlist, kicklist))
+
+        for word in words:
             if not word in self.regex:
                 # Clean up the word from special caracters that might make a word seen as two separate words.
                 # e.g. "c'est" is seen as separate "c" and "est", which is not what we want.
@@ -258,7 +217,11 @@ class Badwords(callbacks.Plugin):
                 regex = r"\b%s\b" % fnmatch.translate(clean.decode("utf-8"))[:-1]
                 self.regex[word] = re.compile(regex, re.IGNORECASE | re.UNICODE)
             if self.regex[word].search(re.sub("['_-]", "", txt.decode("utf-8"))):
-                return irc.reply(self.responseString, private=self.responseAsPrivate, notice=self.responseAsNotice)
+                kick = words[word]
+                if kick:
+                    irc.sendMsg(ircmsgs.kick(channel, msg.nick, self.registryValue('response.string')))
+                else:
+                    irc.reply(self.registryValue('response.string'), private=self.registryValue('response.asPrivate'), notice=self.registryValue('response.asNotice'))
 
 Class = Badwords
 
